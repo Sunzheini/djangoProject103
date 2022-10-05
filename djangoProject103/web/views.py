1e6faca4e0f27da1673c0235f2e6fef42af3e7e6
Sunzheini <daniel_zorov@abv.bg>
2022-10-05 17:17:33 +0300

    Update05102022

--- djangoProject103/web/views.py
+++ djangoProject103/web/views.py
@@ -4,11 +4,26 @@ from django.http import HttpResponse
 from django.shortcuts import render
 
 
+class Student:
+    def __init__(self, name, age):
+        self.name = name
+        self.age = age
+
+    def get_info(self):
+        return f"Name: {self.name}; Age: {self.age}"
+
+
 def index(request):
     context = {
         'title': 'SoftUni Homepage',
         'value': random.random(),
         'info': {'address': 'Sofia'},
+        'student': Student('Daniel', 40),
     }
     return render(request, 'index.html', context)
+
+
+
+
+
 
